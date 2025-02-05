7f70632312fc1895bb720c699969ba98d9d68011
Sanjay V Y <167050358+sanjay-v-y@users.noreply.github.com>
2025-02-05 11:53:30 +0530

    test

--- Frontend/pic-lab-portal/src/App.js
+++ Frontend/pic-lab-portal/src/App.js
@@ -5,16 +5,24 @@ import FacultyDashboard from "./assets/Pages/Faculty/Dashboard";
 import IndustryPersonDashboard from "./assets/Pages/IndustryPerson/Dashboard";
 import Login from "./assets/Pages/Auth/Login";
 
+function Home() {
+  return <h1>Yare Yare daze :|</h1>;
+}
+
 function App() {
   return (
     <Router>
+      <div>
+        <h1>Jotaro Kujo</h1>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/student/dashboard" element={<StudentDashboard />} />
         <Route path="/faculty/dashboard" element={<FacultyDashboard />} />
         <Route path="/industry/dashboard" element={<IndustryPersonDashboard />} />
       </Routes>
+      </div>
     </Router>
   );
 }
